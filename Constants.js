b2eed0385ce191a4b382523da3046ae86b431527
MillanUka <41311473+MillanUka@users.noreply.github.com>
2020-01-26 12:00:51 +1300

    Styles changes

--- Constants.js
+++ Constants.js
@@ -1,2 +1,4 @@
 const API = "https://api.github.com/";
 const SEARCH_USER = "search/users?q=";
+const SEARCH_USER_PROFILE_LIST = [];
+const USER_LISt = [];
--- Search.js
+++ Search.js
@@ -5,30 +5,28 @@ document.getElementById("searchbutton").addEventListener("click", function() {
 
 function performSearch(searchedUsername) {
     clearResults();
-    console.log(API + SEARCH_USER + searchedUsername)
+    console.log(API + SEARCH_USER + searchedUsername);
+    $.ajax({
+        type: 'GET',
+        url: API + SEARCH_USER + searchedUsername,
+        dataType: 'jsonp',
+        success: function getProfile(data) {
+            var searchResults = document.getElementById('searchresults');
 
-    try {
-        $.ajax({
-            type: 'GET',
-            url: API + SEARCH_USER + searchedUsername,
-            dataType: 'jsonp',
-            success: function(data) {
-                console.log(data);
+            console.log(data)
+            data.data.items.forEach(userProfile => {
+                SEARCH_USER_PROFILE_LIST.push(userProfile);
+                var userProfileElement = "<a href=\"" +
+                    userProfile.html_url + "\"><div id=\"searcheduser\"><div id=\"userdetails\"><h1>" +
+                    userProfile.login + "</h1> <img src=\"" +
+                    userProfile.avatar_url + "\" alt=\"User profile image\"></div></div></a>";
+                searchResults.insertAdjacentHTML('beforeend', userProfileElement);
+            });
 
-                var searchResults = document.getElementById('searchresults');
-
-                data.data.items.forEach(userProfile => {
-                    var userProfileElement = "<a href=\"" +
-                        userProfile.html_url + "\"><div id=\"searcheduser\"><div id=\"userdetails\"><h1>" +
-                        userProfile.login + "</h1> <img src=\"" +
-                        userProfile.avatar_url + "\" alt=\"User profile image\"></div></div></a>";
-                    searchResults.insertAdjacentHTML('beforeend', userProfileElement);
-                });
-            }
-        });
-    } catch (error) {
-        searchResults.insertAdjacentHTML('beforeend', error);
-    }
+            console.log(SEARCH_USER_PROFILE_LIST);
+        },
+        async: false
+    });
 }
 
 function clearResults() {
