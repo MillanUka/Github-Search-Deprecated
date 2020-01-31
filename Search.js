fe39589eaf1776c47645278027904209b37876a1
MillanUka <41311473+MillanUka@users.noreply.github.com>
2020-02-01 11:27:43 +1300

    Adds Previous and Next Buttons
    Adds Previous and Next Buttons that display the next and previous search results

--- Search.js
+++ Search.js
@@ -1,32 +1,53 @@
 document.getElementById("searchbutton").addEventListener("click", function() {
     console.log("search button clicked")
-    performSearch(document.getElementById("searchbar").value);
+    performSearch(API + SEARCH_USER + document.getElementById("searchbar").value);
 });
 
-function performSearch(searchedUsername) {
+function performSearch(search) {
     clearResults();
-    console.log(API + SEARCH_USER + searchedUsername);
     $.ajax({
         type: 'GET',
-        url: API + SEARCH_USER + searchedUsername,
+        url: search,
         dataType: 'jsonp',
         success: function getProfile(data) {
-            var searchResults = document.getElementById('searchresults');
-
+            nextSearchResultsLinks = data.meta.Link;
             console.log(data)
-            data.data.items.forEach(userProfile => {
-                SEARCH_USER_PROFILE_LIST.push(userProfile);
-                var userProfileElement = "<a href=\"" +
-                    userProfile.html_url + "\"><div class=\"card border\" id=\"searcheduser\"><div class=\"card-title\"id=\"userdetails\" style=\"text-align: center;\"><h1>" +
-                    userProfile.login + "</h1> <img class=\"card-img-top border\" src=\"" +
-                    userProfile.avatar_url + "\" alt=\"User profile image\"></div></div></a>";
-                searchResults.insertAdjacentHTML('beforeend', userProfileElement);
-            });
-
-            console.log(SEARCH_USER_PROFILE_LIST);
+            displaySearchResults(data.data.items);
         },
         async: false
     });
+}
+
+function nextSearchResults() {
+    clearResults();
+    console.log(nextSearchResultsLinks);
+    if (nextSearchResultsLinks[0][0] != null && nextSearchResultsLinks[0][1].rel == "next")
+        performSearch(nextSearchResultsLinks[0][0]);
+    else if (nextSearchResultsLinks[1][0] != null && nextSearchResultsLinks[1][1].rel == "next") {
+        performSearch(nextSearchResultsLinks[1][0]);
+    }
+}
+
+function prevSearchResults() {
+    clearResults();
+    console.log(nextSearchResultsLinks);
+    if (nextSearchResultsLinks[0][0] != null && nextSearchResultsLinks[0][1].rel == "prev")
+        performSearch(nextSearchResultsLinks[0][0]);
+}
+
+function displaySearchResults(items) {
+    var searchResults = document.getElementById('searchresults');
+    items.forEach(userProfile => {
+        var userProfileElement = "<a href=\"" +
+            userProfile.html_url + "\"><div class=\"card border\" id=\"searcheduser\"><div class=\"card-title\"id=\"userdetails\" style=\"text-align: center;\"><h1>" +
+            userProfile.login + "</h1> <img class=\"card-img-top border\" src=\"" +
+            userProfile.avatar_url + "\" alt=\"User profile image\"></div></div></a>";
+        searchResults.insertAdjacentHTML('beforeend', userProfileElement);
+    });
+
+    searchResults.insertAdjacentHTML('beforeend', "<button class=\"btn bg-primary text-white\" id=\"prev\">Prev</button><button class=\"btn bg-primary text-white\" id=\"next\">Next</button>");
+    document.getElementById("next").addEventListener("click", nextSearchResults);
+    document.getElementById("prev").addEventListener("click", prevSearchResults);
 }
 
 function clearResults() {
